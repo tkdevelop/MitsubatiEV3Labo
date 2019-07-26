090c14ba3a73c6247cb5cdc4a4dda41391a22c75
Toshiaki Kosuga <t.kosuga1741@morijyobi.ac.jp>
2019-07-26 14:14:44 +0900

    自己位置推定クラス作成しました。未実装

--- app/include/distance.h
+++ app/include/distance.h
@@ -3,12 +3,16 @@
 
 typedef struct {
 	float distance; /* 走行距離 */
+	float distance4msL; /* 左タイヤの4ms間の距離 */
+	float distance4msR; /* 右タイヤの4ms間の距離 */
 
 	float preAngleLeft; /* 前回の左モータ回転角度 */
 	float preAngleRight; /* 前回の右モータ回転角度 */
 }Distance;
 void Distance_init(Distance* self);
 void Distance_update(Distance* self);
-void Distance_get_distance();
+float Distance_get_distance(Distance* self);
+float Distance_get_distance_4ms_right(Distance* self);
+float Distance_get_distance_4ms_left(Distance* self);
 
 #endif
---
+++ app/src/distance.c
@@ -0,0 +1,54 @@
+#include "ev3api.h"
+#include "distance.h"
+
+/* 左右モーターポート */
+#define LEFT_MOTOR_P LEFT_MOTOR
+#define RIGHT_MOTOR_P RIGHT_MOTOR
+
+/*
+ * 自己位置推定初期化メソッド
+ *
+ * @param self 自分のポインタ
+ */
+void Distance_init(Distance* self) {
+
+}
+
+/*
+ * 距離更新メソッド
+ *
+ * @param self 自分のポインタ
+ */
+void Distance_update(Distance* self) {
+
+}
+
+/*
+ * 走行距離取得メソッド
+ *
+ * @param self 自分のポインタ
+ * return distance 走行距離 
+ */
+float Distance_get_distance(Distance* self) {
+	return 0;
+}
+
+/*
+ * 左タイヤの4ms間の距離取得メソッド
+ *
+ * @param self 自分のポインタ
+ * return distance4msL 左タイヤの4ms間の距離
+ */
+float Distance_get_distance_4ms_left(Distance* self) {
+	return 0;
+}
+
+/*
+ * 右タイヤの4ms間距離取得メソッド
+ *
+ * @param self 自分のポインタ
+ * return distance4msR 右タイヤの4ms間の距離
+ */
+float Distance_get_distance_4ms_right(Distance* self) {
+	return 0;
+}
